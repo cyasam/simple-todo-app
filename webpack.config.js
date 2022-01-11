53826157caca17bef1245d462750a8ee1056ec82
Cagdas ISIK <cyasam@gmail.com>
2022-01-11 04:33:58 +0300

    Webpack configurations changed.

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,56 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: process.env.NODE_ENV || 'development',
-  entry: {
-    app: ['whatwg-fetch', './src/index.js'],
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].min.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  useBuiltIns: 'usage',
-                  corejs: 3,
-                },
+module.exports = (env) => {
+  return {
+    mode: env.production ? 'production' : 'development',
+    entry: {
+      app: ['whatwg-fetch', './src/index.js'],
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].min.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    useBuiltIns: 'usage',
+                    corejs: 3,
+                  },
+                ],
               ],
-            ],
+            },
           },
         },
-      },
+      ],
+    },
+    optimization: {
+      minimizer: [`...`, new CssMinimizerPlugin()],
+    },
+    devtool: !env.production ? 'eval-source-map' : false,
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].min.css',
+      }),
+      new HtmlWebpackPlugin({
+        inject: 'body',
+        template: './public/index.html',
+      }),
     ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].min.css',
-    }),
-    new HtmlWebpackPlugin({
-      inject: 'body',
-      template: './public/index.html',
-    }),
-  ],
+  };
 };
